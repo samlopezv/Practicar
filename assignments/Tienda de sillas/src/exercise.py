5ba45df3db1286a2a133e4302f044e8c4d53b0c0
samlopezv <a01742394@itesm.mx>
2021-08-26 20:57:06 +0000

    Sillas

---
+++ assignments/Tienda de sillas/src/exercise.py
@@ -0,0 +1,33 @@
+import math
+
+def silla(s):
+    if s == 'B':
+        return int(700)
+    elif s == 'E':
+        return int(900)
+    elif s == 'L':
+        return int(1500)
+
+
+def cielnte(c):
+    if c == 'F':
+       return 0.2
+   
+def descu(d):
+    if total >= 10000 and <20000
+        totald = total
+
+def main():
+    #escribe tu código abajo de esta línea
+    s = input('Tipo de silla B(básica), E(estándar), L(lujo): ')
+    silla(s)
+    c = input('Ingresa el tipo de cliente F(frecuente) o N(normal): ')
+    tot = int(input('Cantidad de sillas: '))
+    total = tot*s
+
+
+  
+
+
+if __name__=='__main__':
+    main()
