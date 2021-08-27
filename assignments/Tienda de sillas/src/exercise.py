2511c2460b6931ceeed8a47b81ad2a203307b8fb
samlopezv <a01742394@itesm.mx>
2021-08-27 01:12:16 +0000

    Resolviendo

--- assignments/Tienda de sillas/src/exercise.py
+++ assignments/Tienda de sillas/src/exercise.py
@@ -12,18 +12,53 @@ def silla(s):
 def cielnte(c):
     if c == 'F':
        return 0.2
+    elif c == 'N':
+        return 0
    
 def descu(d):
-    if total >= 10000 and <20000
-        totald = total
+    if (d >= 10000) and (d <20000):
+        return 0.10
+    elif d>= 20000:
+        return 0.15
+    else:
+        return 0
+        
 
 def main():
-    #escribe tu código abajo de esta línea
+    #Preguntas
     s = input('Tipo de silla B(básica), E(estándar), L(lujo): ')
-    silla(s)
     c = input('Ingresa el tipo de cliente F(frecuente) o N(normal): ')
     tot = int(input('Cantidad de sillas: '))
-    total = tot*s
+
+    #Precio normal
+    silla (s)
+    total = s*tot
+
+    #Descuento de compra
+    d = float(total)
+    descu(d)
+    desc1 = total*d
+    totaldesc1 = total - desc1
+
+
+    #Descuento cliente
+    cliente (c)
+    desc2 = totaldesc1*c
+    totaldesc2 = totaldesc1 - desc2
+
+    #Suma descuentos
+    desctotal = desc1 + desc2
+
+    comprob = total - desctotal
+
+
+
+    #Resultados
+    print(f'Precio antes del descuento: {total}')
+    print(f'Descuento: {desctotal}')
+    print(f'Total a pagar: {totaldesc2}')
+    print(f'Comprobación {comprob}')
+
 
 
   
